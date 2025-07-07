9abac8b4449031b87ad128bd222549682d097ccc
Somaiya Noori <somayenoori786@icloud.com>
2025-07-07 17:18:17 +0430

    just write some code to start my server

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+//EXPRESS
+const app = express();
+const PORT = process.env.PORT || 3008;
+
+app.get("/", (req, res) => {
+  res.send("Foodie Hub API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on this PORT ${PORT}`);
+});
