5018887bf93f1e96642f5c8a6640e9b9f534f720
Somaiya Noori <somayenoori786@icloud.com>
2025-07-11 21:38:01 +0430

    create menuItem Routes

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,61 +1,27 @@
-import pool from "../db/index.js";
+import express from "express";
+import {
+  getRestaurantMenu,
+  createMenuItem,
+  editMenuItem,
+  removeMenuItem,
+  changeAvailability,
+} from "../controllers/menuController.js";
 
-// Get all menu items for a specific restaurant
-export const getMenuItemsByRestaurant = async (restaurantId) => {
-  const result = await pool.query(
-    "SELECT * FROM menu_items WHERE restaurant_id = $1",
-    [restaurantId]
-  );
-  return result.rows;
-};
+const router = express.Router();
 
-// Add new menu item
-export const addMenuItem = async ({
-  name,
-  price,
-  available = true,
-  restaurant_id,
-}) => {
-  const result = await pool.query(
-    `INSERT INTO menu_items (name, price, available, restaurant_id)
-     VALUES ($1, $2, $3, $4)
-     RETURNING *`,
-    [name, price, available, restaurant_id]
-  );
-  return result.rows[0];
-};
+//  all menu items
+router.get("/restaurants/:id/menu", getRestaurantMenu);
 
-// Update it
-export const updateMenuItem = async (id, { name, price, available }) => {
-  const result = await pool.query(
-    `UPDATE menu_items
-     SET name = $1, price = $2, available = $3
-     WHERE id = $4
-     RETURNING *`,
-    [name, price, available, id]
-  );
-  return result.rows[0];
-};
+// new menu item
+router.post("/restaurants/:id/menu", createMenuItem);
 
-// Delete it
-export const deleteMenuItem = async (id) => {
-  const result = await pool.query(
-    `DELETE FROM menu_items
-     WHERE id = $1
-     RETURNING *`,
-    [id]
-  );
-  return result.rows[0];
-};
+// update
+router.put("/menu/:id", editMenuItem);
 
-// Set availability (true/false)
-export const setMenuItemAvailable = async (id, available) => {
-  const result = await pool.query(
-    `UPDATE menu_items
-     SET available = $1
-     WHERE id = $2
-     RETURNING *`,
-    [available, id]
-  );
-  return result.rows[0];
-};
+// DELETE
+router.delete("/menu/:id", removeMenuItem);
+
+// PATCH
+router.patch("/menu/:id/availability", changeAvailability);
+
+export default router;
